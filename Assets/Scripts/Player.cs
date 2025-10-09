0a64209f71ebcf3d774309020418ecb6280efab4
Sol4k <lukasz.drobik6@gmail.com>
2025-10-09 10:36:00 +0200

    koniec tutorial

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,18 @@
 using Fusion;
+using TMPro;
 using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
     [SerializeField] private Ball _prefabBall;
     [SerializeField] private PhysxBall _prefabPhysxBall;
+    public Material _material;
+
 
     [Networked] private TickTimer delay { get; set; }
+    [Networked]
+    public bool spawnedProjectile { get; set; }
+    [Networked] private TickTimer messageLife { get; set; }
 
     private NetworkCharacterController _cc;
     private Vector3 _forward;
@@ -15,7 +21,9 @@ public class Player : NetworkBehaviour
     {
         _cc = GetComponent<NetworkCharacterController>();
         _forward = transform.forward;
+        _material = GetComponentInChildren<MeshRenderer>().material;
     }
+
 
     public override void FixedUpdateNetwork()
     {
@@ -40,6 +48,7 @@ public class Player : NetworkBehaviour
                       {
                           // Initialize the Ball before synchronizing it
                           o.GetComponent<Ball>().Init();
+                          spawnedProjectile = !spawnedProjectile;
                       });
                 }
                 else if (data.buttons.IsSet(NetworkInputData.MOUSEBUTTON1))
@@ -51,10 +60,65 @@ public class Player : NetworkBehaviour
                       Object.InputAuthority,
                       (runner, o) =>
                       {
+                          spawnedProjectile = !spawnedProjectile;
                           o.GetComponent<PhysxBall>().Init(10 * _forward);
                       });
                 }
             }
         }
+
+    }
+    private ChangeDetector _changeDetector;
+
+    public override void Spawned()
+    {
+        _changeDetector = GetChangeDetector(ChangeDetector.Source.SimulationState);
+    }
+    public override void Render()
+    {
+        foreach (var change in _changeDetector.DetectChanges(this))
+        {
+            switch (change)
+            {
+                case nameof(spawnedProjectile):
+                    _material.color = Color.white;
+                    break;
+            }
+        }
+        _material.color = Color.Lerp(_material.color, Color.blue, Time.deltaTime);
+       
+    }
+    private void Update()
+    {
+        if (Object.HasInputAuthority && Input.GetKeyDown(KeyCode.R))
+        {
+            RPC_SendMessage("Hey Mate!");
+        }
+    }
+    private TMP_Text _messages;
+  
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority, HostMode = RpcHostMode.SourceIsHostPlayer)]
+    public void RPC_SendMessage(string message, RpcInfo info = default)
+    {
+        RPC_RelayMessage(message, info.Source);
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All, HostMode = RpcHostMode.SourceIsServer)]
+    public void RPC_RelayMessage(string message, PlayerRef messageSource)
+    {
+        if (_messages == null)
+            _messages = FindObjectOfType<TMP_Text>();
+
+        if (messageSource == Runner.LocalPlayer)
+        {
+            message = $"You said: {message}\n";
+        }
+        else
+        {
+            message = $"Some other player said: {message}\n";
+        }
+
+        _messages.text += message;
+        
     }
 }
